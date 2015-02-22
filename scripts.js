d33e0ee448547eea2ef48802c7903daf020ccc0a
lunete <claud@openmailbox.org>
2015-02-21 21:18:41 -0500

    moving everything from our former directory

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//this still needs to be integrated with jquery
+$(function() {
+    $('.dropdown ul li').on('click', function() {
+        var label = $(this).parent().parent().children('label');
+        label.attr('data-value', $(this).attr('data-value'));
+        label.html($(this).html());
+
+        $(this).parent().children('selected').removeClass('selected');
+        $(this).addClass('selected');
+    });
+});
